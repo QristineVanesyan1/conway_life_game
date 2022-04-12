7b296f364c7fd17d537952cbad584c692c67cf50
Christine Vanesyan <80003543+QristineVanesyan1@users.noreply.github.com>
2022-04-12 23:08:20 +0400

    Add files via upload

---
+++ Example.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conway_life_game
+{
+    static class Example
+    {
+        internal static int[,] arr;
+        internal static void InitPulsar(int size)
+        {
+            arr = new int[size, size];
+
+            arr[4, 2] = 1;
+            arr[4, 7] = 1;
+            arr[4, 9] = 1;
+            arr[4, 14] = 1;
+
+            arr[5, 2] = 1;
+            arr[5, 7] = 1;
+            arr[5, 9] = 1;
+            arr[5, 14] = 1;
+
+            arr[6, 2] = 1;
+            arr[6, 7] = 1;
+            arr[6, 9] = 1;
+            arr[6, 14] = 1;
+
+            arr[10, 2] = 1;
+            arr[10, 7] = 1;
+            arr[10, 9] = 1;
+            arr[10, 14] = 1;
+
+            arr[11, 2] = 1;
+            arr[11, 7] = 1;
+            arr[11, 9] = 1;
+            arr[11, 14] = 1;
+
+            arr[12, 2] = 1;
+            arr[12, 7] = 1;
+            arr[12, 9] = 1;
+            arr[12, 14] = 1;
+
+            arr[2, 4] = 1;
+            arr[2, 5] = 1;
+            arr[2, 6] = 1;
+            arr[2, 10] = 1;
+            arr[2, 11] = 1;
+            arr[2, 12] = 1;
+
+            arr[7, 4] = 1;
+            arr[7, 5] = 1;
+            arr[7, 6] = 1;
+            arr[7, 10] = 1;
+            arr[7, 11] = 1;
+            arr[7, 12] = 1;
+
+            arr[9, 4] = 1;
+            arr[9, 5] = 1;
+            arr[9, 6] = 1;
+            arr[9, 10] = 1;
+            arr[9, 11] = 1;
+            arr[9, 12] = 1;
+
+            arr[14, 4] = 1;
+            arr[14, 5] = 1;
+            arr[14, 6] = 1;
+            arr[14, 10] = 1;
+            arr[14, 11] = 1;
+            arr[14, 12] = 1;
+        }
+
+      
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading;
+
+namespace conway_life_game
+{
+    class Program
+    {
+        static int size = 30;
+        static void Show(int[,] arr)
+        {
+            Console.Clear();
+            for (int j = 0; j < size; j++)
+            {
+                for (int i = 0; i < size; i++)
+                {
+
+                    if (arr[i, j] != 0)
+                    {
+                        Console.Write($"*");
+                    }
+                    else
+                    {
+                        Console.Write($" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Thread.Sleep(1000);
+        }
+
+        static int NeighborhoodCount(int[,] arr, int x, int y)
+        {
+
+            if (x == 0 || y == 0 || size == 4 || size == 4)
+            {
+                return 0;
+            }
+
+            return  arr[x - 1, y + 1] +
+                    arr[x + 1, y - 1] +
+                    arr[x - 1, y - 1] +
+                    arr[x + 1, y + 1] + 
+                    arr[x - 1, y] +
+                    arr[x + 1, y] +
+                    arr[x, y - 1] + 
+                    arr[x, y + 1];
+        }
+
+        static void ConwayLifeCalculation(int[,] arr,int[,] temp,int i, int j, int count)
+        {
+            if (count <= 1)
+            {
+                temp[i, j] = 0;
+            }
+
+            if (count >= 4)
+            {
+                temp[i, j] = 0;
+            }
+
+            if (count == 2 || count == 3)
+            {
+                if (arr[i, j] == 1)
+                {
+                    temp[i, j] = 1;
+                }
+            }
+
+            if (arr[i, j] == 0)
+            {
+                if (count == 3)
+                {
+
+                    temp[i, j] = 1;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Example.InitPulsar(size);
+
+            int[,] arr = Example.arr;
+            int[,] temp = new int[size, size];
+
+            while (true)
+            {
+                for (int j = 0; j < size-1; j++)
+                {
+                    for (int i = 0; i < size-1; i++)
+                    { 
+                        var count = NeighborhoodCount(arr, i, j);
+                        ConwayLifeCalculation(arr,temp,i,j,count);
+                    }
+                }
+               
+               for (int j = 0; j < size; j++)
+                {
+                    for (int i = 0; i < size; i++)
+                    {
+                        arr[i, j] = temp[i, j];
+                    }
+                }
+
+                Show(temp);
+           }
+        }
+    }
+}
